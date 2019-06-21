71fea38bd2a60f39bf747d75599620f4b71c0a6f
williambergmann <williebergmann@gmail.com>
2019-06-20 20:58:24 -0500

    file changes added

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 4001;
 
+app.get('/', (req, res) => {
+    res.send()
+})
+
 app.listen(port, () => {
  console.log(`Web server is listening on port ${port}!`);
 });
